6c08bed5dfe98ae8611078c78366b759b8241922
Dan Vaynshteyn <danvayn@gmail.com>
2014-03-11 09:21:15 -0500

    Lab 1 completed
    reservester will be updated for lab 2 later.

--- README.md
+++
@@ -1,2 +0,0 @@
-Spring2014DanielVaynshteyn
-==========================
---
+++ reservester/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reservester::Application.config.secret_key_base = '76bbcbd97988bd29ef2fca3e43977e4f8b0e4b0c59714fc94c1d02f49b38d2b112c6a9a2239f445310fd03616d39479c08620bf25d219f96865c9c248102539e'
